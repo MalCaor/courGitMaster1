ddaabb70a68ba7756038430d4c6bb2d9d417a396
MalCaor <xav.lemen@orange.fr>
2023-01-11 08:24:01 +0100

    add comment

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
+// Hello word
 console.log('Hello, world!');
 
+// addition
 const operand = 12;
 const operand2 = 45;
 
@@ -7,6 +9,7 @@ let result = operand + operand2;
 
 console.log("resultat de" + operand + "+" + operand2 + "=" + result);
 
+// aPerson say hello
 let aPerson = { 
 	name: 'Jean',
 	age: 35,
